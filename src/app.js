6d97d01cc16eb685fb4951b01b66a49937fe8b8e
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-08-28 14:01:05 +0100

    dfdf

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './app.scss';
-import Welcome from './components/views/Welcome/Welcome';
 import NotFound from './components/views/NotFound/NotFound';
 import Header from './components/common/header';
+import Card from './components/common/landingPage/cards';
 
 const App = () => (
   <Provider store={store}>
@@ -13,7 +13,7 @@ const App = () => (
       <Header />
       <div className="container">
         <Switch>
-          <Route exact path="/" component={Welcome} />
+          <Route exact path="/" component={Card} />
           <Route path="*" component={NotFound} />
         </Switch>
       </div>
